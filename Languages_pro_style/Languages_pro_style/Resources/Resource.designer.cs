3454ea8eafe21ed871b87535e22e9bc6fcab7b40
Messering <messering@yandex.ru>
2015-06-30 19:03:37 +0300

    add packeg sqllite and add butto back menu test

--- Languages_pro_style/Languages_pro_style/Resources/Resource.designer.cs
+++ Languages_pro_style/Languages_pro_style/Resources/Resource.designer.cs
@@ -72,68 +72,74 @@ namespace Languages_pro_style
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060010
-			public const int buttonDictionary = 2131099664;
-			
 			// aapt resource value: 0x7f060012
-			public const int buttonRT = 2131099666;
-			
-			// aapt resource value: 0x7f06000e
-			public const int buttonRepeatW = 2131099662;
-			
-			// aapt resource value: 0x7f06000c
-			public const int button_LearNW = 2131099660;
-			
-			// aapt resource value: 0x7f060004
-			public const int imageView1 = 2131099652;
-			
-			// aapt resource value: 0x7f060002
-			public const int tableLayout1 = 2131099650;
-			
-			// aapt resource value: 0x7f06000a
-			public const int tableLayout2 = 2131099658;
-			
-			// aapt resource value: 0x7f060003
-			public const int tableRow1 = 2131099651;
-			
-			// aapt resource value: 0x7f060006
-			public const int tableRow2 = 2131099654;
-			
-			// aapt resource value: 0x7f060008
-			public const int tableRow3 = 2131099656;
-			
-			// aapt resource value: 0x7f06000b
-			public const int tableRow4 = 2131099659;
-			
-			// aapt resource value: 0x7f06000d
-			public const int tableRow5 = 2131099661;
-			
-			// aapt resource value: 0x7f06000f
-			public const int tableRow6 = 2131099663;
-			
-			// aapt resource value: 0x7f060011
-			public const int tableRow7 = 2131099665;
-			
-			// aapt resource value: 0x7f060000
-			public const int textViewDictionary = 2131099648;
-			
-			// aapt resource value: 0x7f060001
-			public const int textViewLearnNW = 2131099649;
-			
-			// aapt resource value: 0x7f060013
-			public const int textViewRT = 2131099667;
+			public const int buttonDictionary = 2131099666;
 			
 			// aapt resource value: 0x7f060014
-			public const int textViewRepearW = 2131099668;
+			public const int buttonRT = 2131099668;
 			
-			// aapt resource value: 0x7f060007
-			public const int textView_PassLes = 2131099655;
+			// aapt resource value: 0x7f060010
+			public const int buttonRepeatW = 2131099664;
 			
-			// aapt resource value: 0x7f060009
-			public const int textView_PassWords = 2131099657;
+			// aapt resource value: 0x7f06000e
+			public const int button_LearNW = 2131099662;
+			
+			// aapt resource value: 0x7f060000
+			public const int buttonback = 2131099648;
+			
+			// aapt resource value: 0x7f060001
+			public const int checkedTextView1 = 2131099649;
+			
+			// aapt resource value: 0x7f060006
+			public const int imageView1 = 2131099654;
+			
+			// aapt resource value: 0x7f060004
+			public const int tableLayout1 = 2131099652;
+			
+			// aapt resource value: 0x7f06000c
+			public const int tableLayout2 = 2131099660;
 			
 			// aapt resource value: 0x7f060005
-			public const int textView_UserName = 2131099653;
+			public const int tableRow1 = 2131099653;
+			
+			// aapt resource value: 0x7f060008
+			public const int tableRow2 = 2131099656;
+			
+			// aapt resource value: 0x7f06000a
+			public const int tableRow3 = 2131099658;
+			
+			// aapt resource value: 0x7f06000d
+			public const int tableRow4 = 2131099661;
+			
+			// aapt resource value: 0x7f06000f
+			public const int tableRow5 = 2131099663;
+			
+			// aapt resource value: 0x7f060011
+			public const int tableRow6 = 2131099665;
+			
+			// aapt resource value: 0x7f060013
+			public const int tableRow7 = 2131099667;
+			
+			// aapt resource value: 0x7f060002
+			public const int textViewDictionary = 2131099650;
+			
+			// aapt resource value: 0x7f060003
+			public const int textViewLearnNW = 2131099651;
+			
+			// aapt resource value: 0x7f060015
+			public const int textViewRT = 2131099669;
+			
+			// aapt resource value: 0x7f060016
+			public const int textViewRepearW = 2131099670;
+			
+			// aapt resource value: 0x7f060009
+			public const int textView_PassLes = 2131099657;
+			
+			// aapt resource value: 0x7f06000b
+			public const int textView_PassWords = 2131099659;
+			
+			// aapt resource value: 0x7f060007
+			public const int textView_UserName = 2131099655;
 			
 			static Id()
 			{
--- Languages_pro_style/Languages_pro_style/obj/Release/android/com/companyname/languages_pro_style/R.java
+++ Languages_pro_style/Languages_pro_style/obj/Release/android/com/companyname/languages_pro_style/R.java
@@ -18,27 +18,29 @@ public final class R {
         public static final int monoandroidsplash=0x7f020004;
     }
     public static final class id {
-        public static final int buttonDictionary=0x7f060010;
-        public static final int buttonRT=0x7f060012;
-        public static final int buttonRepeatW=0x7f06000e;
-        public static final int button_LearNW=0x7f06000c;
-        public static final int imageView1=0x7f060004;
-        public static final int tableLayout1=0x7f060002;
-        public static final int tableLayout2=0x7f06000a;
-        public static final int tableRow1=0x7f060003;
-        public static final int tableRow2=0x7f060006;
-        public static final int tableRow3=0x7f060008;
-        public static final int tableRow4=0x7f06000b;
-        public static final int tableRow5=0x7f06000d;
-        public static final int tableRow6=0x7f06000f;
-        public static final int tableRow7=0x7f060011;
-        public static final int textViewDictionary=0x7f060000;
-        public static final int textViewLearnNW=0x7f060001;
-        public static final int textViewRT=0x7f060013;
-        public static final int textViewRepearW=0x7f060014;
-        public static final int textView_PassLes=0x7f060007;
-        public static final int textView_PassWords=0x7f060009;
-        public static final int textView_UserName=0x7f060005;
+        public static final int buttonDictionary=0x7f060012;
+        public static final int buttonRT=0x7f060014;
+        public static final int buttonRepeatW=0x7f060010;
+        public static final int button_LearNW=0x7f06000e;
+        public static final int buttonback=0x7f060000;
+        public static final int checkedTextView1=0x7f060001;
+        public static final int imageView1=0x7f060006;
+        public static final int tableLayout1=0x7f060004;
+        public static final int tableLayout2=0x7f06000c;
+        public static final int tableRow1=0x7f060005;
+        public static final int tableRow2=0x7f060008;
+        public static final int tableRow3=0x7f06000a;
+        public static final int tableRow4=0x7f06000d;
+        public static final int tableRow5=0x7f06000f;
+        public static final int tableRow6=0x7f060011;
+        public static final int tableRow7=0x7f060013;
+        public static final int textViewDictionary=0x7f060002;
+        public static final int textViewLearnNW=0x7f060003;
+        public static final int textViewRT=0x7f060015;
+        public static final int textViewRepearW=0x7f060016;
+        public static final int textView_PassLes=0x7f060009;
+        public static final int textView_PassWords=0x7f06000b;
+        public static final int textView_UserName=0x7f060007;
     }
     public static final class layout {
         public static final int dictionaryscreen=0x7f030000;

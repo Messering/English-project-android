162c42a8ee487a043d0cbbb84928a03bcf70b8c1
Messering <messering@yandex.ru>
2015-06-30 03:54:50 +0300

    change project
    підлаштував налаштування під емулятор, змінив іконку, пробував
    підключати базу даних, розібрався із структурою

--- Languages_pro_style/Languages_pro_style/obj/Debug/android/src/com/companyname/languages_pro_style/R.java
+++ Languages_pro_style/Languages_pro_style/obj/Debug/android/src/com/companyname/languages_pro_style/R.java
@@ -11,31 +11,34 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int icon1=0x7f020001;
+        public static final int ic_dictionary=0x7f020000;
+        public static final int ic_menu_search=0x7f020001;
+        public static final int icon=0x7f020002;
+        public static final int icon1=0x7f020003;
+        public static final int monoandroidsplash=0x7f020004;
     }
     public static final class id {
-        public static final int buttonDictionary=0x7f050010;
-        public static final int buttonRT=0x7f050012;
-        public static final int buttonRepeatW=0x7f05000e;
-        public static final int button_LearNW=0x7f05000c;
-        public static final int imageView1=0x7f050004;
-        public static final int tableLayout1=0x7f050002;
-        public static final int tableLayout2=0x7f05000a;
-        public static final int tableRow1=0x7f050003;
-        public static final int tableRow2=0x7f050006;
-        public static final int tableRow3=0x7f050008;
-        public static final int tableRow4=0x7f05000b;
-        public static final int tableRow5=0x7f05000d;
-        public static final int tableRow6=0x7f05000f;
-        public static final int tableRow7=0x7f050011;
-        public static final int textViewDictionary=0x7f050000;
-        public static final int textViewLearnNW=0x7f050001;
-        public static final int textViewRT=0x7f050013;
-        public static final int textViewRepearW=0x7f050014;
-        public static final int textView_PassLes=0x7f050007;
-        public static final int textView_PassWords=0x7f050009;
-        public static final int textView_UserName=0x7f050005;
+        public static final int buttonDictionary=0x7f060010;
+        public static final int buttonRT=0x7f060012;
+        public static final int buttonRepeatW=0x7f06000e;
+        public static final int button_LearNW=0x7f06000c;
+        public static final int imageView1=0x7f060004;
+        public static final int tableLayout1=0x7f060002;
+        public static final int tableLayout2=0x7f06000a;
+        public static final int tableRow1=0x7f060003;
+        public static final int tableRow2=0x7f060006;
+        public static final int tableRow3=0x7f060008;
+        public static final int tableRow4=0x7f06000b;
+        public static final int tableRow5=0x7f06000d;
+        public static final int tableRow6=0x7f06000f;
+        public static final int tableRow7=0x7f060011;
+        public static final int textViewDictionary=0x7f060000;
+        public static final int textViewLearnNW=0x7f060001;
+        public static final int textViewRT=0x7f060013;
+        public static final int textViewRepearW=0x7f060014;
+        public static final int textView_PassLes=0x7f060007;
+        public static final int textView_PassWords=0x7f060009;
+        public static final int textView_UserName=0x7f060005;
     }
     public static final class layout {
         public static final int dictionaryscreen=0x7f030000;
@@ -47,5 +50,8 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+    }
+    public static final class style {
+        public static final int Mono_Android_Theme_Splash=0x7f050000;
     }
 }
